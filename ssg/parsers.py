d03ad4a37afa51ed9fb0337a5dd7dd88c57e3f88
amitparikh <35794224+amiparik@users.noreply.github.com>
2020-12-18 22:14:50 +0530

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,8 +1,9 @@
 import sys
+import shutil
 from docutils.core import publish_parts
 from markdown import markdown
 from ssg.content import Content
-import shutil
+
 from typing import List
 from pathlib import Path
 
@@ -38,17 +39,17 @@ class ResourceParser(Parser):
 class MarkdownParser(Parser):
     extensions = [".md", ".markdown"]
 
-    def parse(self, path: Path, source: Path, dest: Path):
+    def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        self.write(path,dest,html)
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name,content)
+        self.write(path, dest, html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 class ReStructuredTextParser(Parser):
     extensions = [".rst"]
 
-    def parse(self, path: Path, source: Path, dest: Path):
+    def parse(self, path, source, dest):
         content = Content.load(self.read(path))
-        html = publish_parts(content.body,writer_name="html5")
-        self.write(path,dest,html["html_body"])
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name,content)
+        html = publish_parts(content.body, writer_name="html5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
